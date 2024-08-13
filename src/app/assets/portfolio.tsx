95bf73e8143db4e56c23a9e0fb7255f2d944f42b
BearMigiano <bearmigiano@berachain.com>
2024-08-13 21:53:34 +0200

    assets page

---
+++ src/app/assets/portfolio.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { TokenRecord } from "./token";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/dialog";
+import { Link } from "lucide-react";
+
+export const Portfolio = ({
+
+}: {
+
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+
+
+<div className="overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 border-b border-blue-600">
+        <thead className="text-xs text-white uppercase border-b border-blue-600  ">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Token
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Amount
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    USD Value
+                </th>
+                <th scope="col" className="px-6 py-3">
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <TokenRecord token="HONEY" price={1.001} amount={1020095.2063} src="/assets/honey.png"/>
+            <TokenRecord token="BERA" price={100000.7} amount={1000} src="/assets/bera.png"/>
+            <TokenRecord token="WETH" price={3424.7} amount={102.2063} src="/assets/weth.png"/>
+
+        </tbody>
+    </table>
+</div>
+
+);
+};
---
+++ src/app/assets/token.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Image from "next/image";
+
+export const TokenRecord = ({
+  token,
+  price,
+  amount,
+  src
+}: {
+  token: string;
+  price: number;
+  amount: number;
+  src: string;
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <tr className=" border-b border-blue-600 hover:bg-blue-900 transition-all duration-300">
+        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <div className="flex items-center gap-4">
+            <Image
+                src={src}
+                alt={token}
+                className="rounded-full w-8 h-8"
+                width={50}
+                height={50}
+            />
+             {token}
+            </div>
+        </th>
+        <td className="px-6 py-4 text-white">
+            ${price}
+        </td>
+        <td className="px-6 py-4 text-white">
+            {amount}
+        </td>
+        <td className="px-6 py-4 text-white">
+            ${amount * price}
+        </td>
+        <td className="px-6 py-4 text-right">
+        <button className="rounded-lg bg-blue-500 p-2">
+            <a href="#" className="font-medium text-white">Invest</a>
+          </button>
+        </td>
+    </tr>
+);
+};
